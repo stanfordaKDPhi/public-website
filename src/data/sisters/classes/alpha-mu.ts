9d40a09e0c4394c3a6c103365428e1b07bdb1f58
christinaduong <christinaduong03@gmail.com>
2026-05-02 15:31:27 -0700

    [sisters] alpha mu class

--- src/data/sisters/classes/alpha-mu.ts
+++ src/data/sisters/classes/alpha-mu.ts
@@ -1,3 +1,53 @@
 import type { SisterClassPageDef } from "../types";
 
-export const alphaMuClassPage: SisterClassPageDef | null = null;
+/**
+ * Add `public/images/sisters/alpha-mu/alpha-mu-group.jpg` (or update `heroImageFile`)
+ * and portraits `295-sidney-suh.*`, etc., before publishing.
+ */
+export const alphaMuClassPage: SisterClassPageDef = {
+  heroImageFile: "alpha-mu-group.JPG",
+  rushTerm: "Winter 2025",
+  heroTitle: "Alpha Mu",
+  members: [
+    {
+      lineNumber: 295,
+      firstName: "Sidney",
+      lastName: "Suh",
+      nickname: "Sid-K",
+      big: "Isabel \"A$AP Pocky\" Mercado",
+      majorMinor: "Political Science",
+      talkToMeAbout:
+        "Sci-Fi books/movies, K-Dramas, K-Hip Hop and R&B, and salt water taffy",
+    },
+    {
+      lineNumber: 296,
+      firstName: "Lizzie",
+      lastName: "Tamor",
+      nickname: "The Notorious L.I.Z.",
+      big: "Alison \"Missy Alliott\" Kim",
+      majorMinor: "Biology, Psychology Minor",
+      talkToMeAbout:
+        "Jewelry, The Vampire Diaries, outfit ideas, and recipes",
+    },
+    {
+      lineNumber: 297,
+      firstName: "Iona",
+      lastName: "Xia",
+      nickname: "Little Miss Chill (Guy)",
+      big: "Lauren \"Little Miss Dreamy\" Yu",
+      majorMinor: "Computer Science and Economics",
+      talkToMeAbout:
+        "Watching figure skating, TV shows, and Gracie Abrams",
+    },
+    {
+      lineNumber: 298,
+      firstName: "Hallie",
+      lastName: "Xu",
+      nickname: "Haylani",
+      big: "Alison \"Missy Alliott\" Kim",
+      majorMinor: "Symbolic Systems",
+      talkToMeAbout:
+        "Absurd dreams, Plants vs. Zombies, tropical fish, and your favorite piece of performance art",
+    },
+  ],
+};
